053be5285509adb91afc57c6bdaf8147d0847a39
Dushyant Singh Chouhan <114542246+Dushyant-1@users.noreply.github.com>
2025-04-16 16:07:09 +0530

    Update main.tf

---
+++ Terraform_Module/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "ffb20f85-a6a5-4637-a47b-3125b82cd858"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-akscluster"
+  location = "East US 2"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "dushyant01"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aksaryan0123456"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "dotnetaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
